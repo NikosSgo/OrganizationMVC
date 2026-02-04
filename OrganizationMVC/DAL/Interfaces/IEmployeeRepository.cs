9b040db39a89253d3b1a4fa8ee9ca7ecbde0b915
NikosSgo <nikosgonnik@gmail.com>
2026-02-04 17:53:06 +0300

    Update interfaces

--- OrganizationMVC/DAL/Interfaces/IEmployeeRepository.cs
+++ OrganizationMVC/DAL/Interfaces/IEmployeeRepository.cs
@@ -4,7 +4,8 @@ namespace OrganizationMVC.DAL.Interfaces;
 
 public interface IEmployeeRepository
 {
-    Task<List<EmployeeEntity>> GetAllByOrganisationIdAsync(int organisationId);
+    Task<(List<EmployeeEntity> Items, int TotalCount)> GetPagedByOrganisationIdAsync(int organisationId, int page, int pageSize);
     Task BatchInsertAsync(List<EmployeeEntity> entities);
-
+    Task BatchUpdateAsync(List<EmployeeEntity> entities);
+    Task BatchDeleteAsync(List<EmployeeEntity> entities);
 }
--- OrganizationMVC/DAL/Interfaces/IOrganizationRepository.cs
+++ OrganizationMVC/DAL/Interfaces/IOrganizationRepository.cs
@@ -4,7 +4,9 @@ namespace OrganizationMVC.DAL.Interfaces;
 
 public interface IOrganizationRepository
 {
-    Task<List<OrganizationEntity>> GetAllAsync();
+    Task<OrganizationEntity?> GetByIdAsync(int id);
+    Task<(List<OrganizationEntity> Items, int TotalCount)> GetPagedAsync(int page, int pageSize);
     Task BatchInsertAsync(List<OrganizationEntity> entities);
-
+    Task BatchUpdateAsync(List<OrganizationEntity> entities);
+    Task BatchDeleteAsync(List<OrganizationEntity> entities);
 }
